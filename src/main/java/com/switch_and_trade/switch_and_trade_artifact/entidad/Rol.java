0f32a79c395cf5ecb22a64838be78a1b30aec2cf
Andres Camacho <andrelomartino@gmail.com>
2022-05-31 19:17:03 -0300

    entidad rol y vehiculo creado

--- src/main/java/com/switch_and_trade/switch_and_trade_artifact/entidad/Rol.java
+++ src/main/java/com/switch_and_trade/switch_and_trade_artifact/entidad/Rol.java
@@ -1,6 +1,23 @@
 package com.switch_and_trade.switch_and_trade_artifact.entidad;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name="rol")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Rol {
+    @Id
+    @GeneratedValue(strategy =IDENTITY)
+    @Column(name="id_rol")
     private Long id;
+    @Column(name="nombre_rol", length = 30, nullable = false)
     private String nombre;
 }
